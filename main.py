ff1d662dfdbaab46fb28bb13d57c134c8f2aa8ac
Paula Ribeiro <paula.mribeiro05@gmail.com>
2021-03-22 18:56:14 -0300

    organiza código e identifica loop

--- main.py
+++ main.py
@@ -1,11 +1,50 @@
-import sys
-import os
+# Autora: Paula Mara Ribeiro
 
 INITIAL_SYMBOL = '1'
 EMPTY_SYMBOL = '11'
 
 DIR_RIGHT = '1'
 DIR_LEFT = '11'
+
+
+def main():
+    mt_input = input('Insira a MT: ')
+    result = process_mt(mt_input)
+    print(result)
+
+
+def process_mt(mt_input):
+    tape_one, tape_two, tape_three = initialize_tapes(mt_input)
+
+    cont = 0
+    finished = False
+    approved = False
+    tape_two_head = 0
+    index_tape_two = 1
+    while cont < 1000 and not finished:
+        r_a = get_corresponding_symbol(tape_two, tape_two_head)
+        r_e = remove_initial_and_final_symbols(tape_three)
+        transition_index = find_transition(tape_one, r_e, r_a)
+        if r_a == EMPTY_SYMBOL or transition_index == -1:
+            finished = True
+            approved = is_final_state(tape_one, r_e)
+        else:
+            new_state, new_symbol, direction = get_transition_info(tape_one, transition_index)
+            tape_three = INITIAL_SYMBOL + new_state + EMPTY_SYMBOL
+            index_tape_two, tape_two = update_tape_two(r_a, tape_two, index_tape_two, new_symbol)
+            tape_two_head = move_tape_two(tape_two_head, direction)
+        cont += 1
+
+    return 'Aprovada' if approved else 'Rejeitada -> entrou em loop' if cont == 1000 else 'Rejeitada'
+
+
+def initialize_tapes(mt_input):
+    tape_one = INITIAL_SYMBOL + mt_input + EMPTY_SYMBOL
+    _, word = mt_input.split('000')
+    tape_two = INITIAL_SYMBOL + word + EMPTY_SYMBOL
+    tape_three = INITIAL_SYMBOL + '1' + EMPTY_SYMBOL
+
+    return tape_one, tape_two, tape_three
 
 
 def find_transition(tape_one, state, symbol):
@@ -28,10 +67,16 @@ def is_final_state(tape_one, state):
 
 
 def get_corresponding_symbol(word, symbol_count):
+    word = remove_initial_and_final_symbols(word)
     symbols = word.split('0')
     if symbol_count >= len(symbols):
         return EMPTY_SYMBOL
     return symbols[symbol_count]
+
+
+def remove_initial_and_final_symbols(tape):
+    return tape[1:len(tape)-2]
+
 
 def move_tape_two(symbol_count, direction):
     if direction == DIR_RIGHT:
@@ -40,41 +85,11 @@ def move_tape_two(symbol_count, direction):
         return symbol_count - 1
 
 
-ex1 = '1110010111011011101001011011101101001011110111011110100101111101110111110100110110111011010011011101110111010011011111011101111101000111011111'
-ex2 = '111001011101101110100101101110110100101111011101111010010111110111011111010011011011101101001101110111011101001101111101110111110100011101111'
-ex3 = '110010111011011101001011110110111101001101110101111011001101111010111011000111101111011101111'
-ex4 = '110010111011011101001011110101111010011011101011110110011011110101110110001111011110111'
-
-def main(argv):
-    mt_input = ex3
-    tape_one = INITIAL_SYMBOL + mt_input + EMPTY_SYMBOL
-    _, word = mt_input.split('000')
-    tape_two = INITIAL_SYMBOL + word + EMPTY_SYMBOL
-    tape_three = INITIAL_SYMBOL + '1' + EMPTY_SYMBOL
-
-    cont = True
-    approved = False
-    symbol_count = 0
-    index_tape_two = 1
-    while cont:
-        r_a = get_corresponding_symbol(tape_two[1:len(tape_two)-2], symbol_count)
-        r_e = tape_three[1:len(tape_three)-2]
-        transition_index = find_transition(tape_one, r_e, r_a)
-        if r_a == EMPTY_SYMBOL:
-            cont = False
-            approved = is_final_state(tape_one, r_e)
-        elif transition_index == -1:
-            cont = False
-            approved = is_final_state(tape_one, r_e)
-        else:
-            new_state, new_symbol, direction = get_transition_info(tape_one, transition_index)
-            tape_three = INITIAL_SYMBOL + new_state + EMPTY_SYMBOL
-            index_tape_two += len(r_a)+1
-            tape_two = tape_two[:index_tape_two] + new_symbol + tape_two[index_tape_two+len(r_a):]
-            symbol_count = move_tape_two(symbol_count, direction)
-
-    print('Aprovada' if approved else 'Rejeitada')
+def update_tape_two(r_a, tape_two, index_tape_two, new_symbol):
+    index_tape_two += len(r_a)+1
+    tape_two = tape_two[:index_tape_two] + new_symbol + tape_two[index_tape_two+len(r_a):]
+    return index_tape_two, tape_two
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
