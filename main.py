44e605b4c7ff6d970c996c5248b38a3df4d95231
Paula Ribeiro <paula.mribeiro05@gmail.com>
2021-03-28 14:04:52 -0300

    fix

--- main.py
+++ main.py
@@ -20,20 +20,20 @@ def process_mt(mt_input):
 
     cont = 0
     approved = False
-    tape_two_head = 0
+    tape_two_head = 1
     while cont < MAX_LOOPS:
         current_symbol = get_corresponding_symbol(tape_two, tape_two_head)
-        current_state = remove_initial_and_final_symbols(tape_three)
-        transition_index = find_transition(tape_one, current_state, current_symbol)
+        current_state = tape_three
+        transition_found, new_state, new_symbol, direction = find_transition(tape_one, current_state, current_symbol)
 
-        if current_symbol == EMPTY_SYMBOL or transition_index == -1:
+        if not transition_found:
             approved = is_final_state(tape_one, current_state)
             break
         else:
-            new_state, new_symbol, direction = get_transition_info(tape_one, transition_index)
             tape_three = update_tape_three(new_state)
             tape_two = update_tape_two(tape_two, tape_two_head, new_symbol)
             tape_two_head = move_tape_two(tape_two_head, direction)
+
             if len(tape_two) >= MAX_TAPE_SIZE:
                 return 'Rejeitada -> excedeu o tamanho da fita ' + str(cont) 
         cont += 1
@@ -42,25 +42,31 @@ def process_mt(mt_input):
 
 
 def initialize_tapes(mt_input):
-    tape_one = INITIAL_SYMBOL + mt_input + EMPTY_SYMBOL
+    tape_one = INITIAL_SYMBOL + '0' + mt_input + '0' + EMPTY_SYMBOL
     _, word = mt_input.split('000')
-    tape_two = INITIAL_SYMBOL + word + EMPTY_SYMBOL
-    tape_three = INITIAL_SYMBOL + '1' + EMPTY_SYMBOL
+    tape_two = INITIAL_SYMBOL + '0' + word + '0' + EMPTY_SYMBOL
+    tape_three = '1'
 
     return tape_one, tape_two, tape_three
 
 
 def find_transition(tape_one, state, symbol):
+    start_transition = tape_one.find('00')
+    end_transition = tape_one.find('000')
+
+    transitions = tape_one[start_transition:end_transition]
+
     f_s = '00' + state + '0' + symbol + '0'
-    index_transition = tape_one.find(f_s)
-    return -1 if index_transition == -1 else index_transition + len(f_s)
-
-
-def get_transition_info(tape_one, transition_index):
-    transition = tape_one[transition_index:len(tape_one)]
-    transition = transition.split('00')[0]
-    new_state, new_symbol, direction = transition.split('0')
-    return new_state, new_symbol, direction
+    index_transition = transitions.find(f_s)
+    
+    if index_transition == -1:
+        return False, 0, 0, 0
+    else:
+        index_transition += len(f_s)
+        transition = transitions[index_transition:end_transition]
+        transition = transition.split('00')[0]
+        new_state, new_symbol, direction = transition.split('0')
+    return True, new_state, new_symbol, direction
 
 
 def is_final_state(tape_one, state):
@@ -70,15 +76,10 @@ def is_final_state(tape_one, state):
 
 
 def get_corresponding_symbol(word, tape_two_head):
-    word = remove_initial_and_final_symbols(word)
     symbols = word.split('0')
     if tape_two_head >= len(symbols):
         return EMPTY_SYMBOL
     return symbols[tape_two_head]
-
-
-def remove_initial_and_final_symbols(tape):
-    return tape[1:len(tape)-2]
 
 
 def move_tape_two(symbol_count, direction):
@@ -89,15 +90,14 @@ def move_tape_two(symbol_count, direction):
 
 
 def update_tape_three(new_state):
-    return INITIAL_SYMBOL + new_state + EMPTY_SYMBOL
+    return new_state
 
 
 def update_tape_two(tape_two, tape_two_head, new_symbol):
-    tape_two = remove_initial_and_final_symbols(tape_two)
     symbols = tape_two.split('0')
     symbols[tape_two_head] = new_symbol
     tape_two = '0'.join(symbols)
-    return INITIAL_SYMBOL + tape_two + EMPTY_SYMBOL
+    return tape_two
 
 
 if __name__ == "__main__":
